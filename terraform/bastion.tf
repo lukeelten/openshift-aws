2c3eb15f6536d9e9b2611c4e6439b63b2601ddc2
Tobias Derksen <tobias.derksen@codecentric.de>
2018-03-05 16:37:20 +0100

    Terraform file for bastion host

---
+++ terraform/bastion.tf
@@ -0,0 +1,21 @@
+resource "aws_instance" "bastion" {
+  ami           = "${data.aws_ami.centos.id}"
+  instance_type = "${var.node-types.bastion}"
+  key_name = "${var.key}"
+
+  vpc_security_group_ids = ["${aws_security_group.bastion-sg.id}"]
+  subnet_id = "${aws_subnet.subnet-public-1.id}"
+
+  tags {
+    Name = "${var.project} - Bastion"
+    Project = "${var.project}"
+  }
+}
+
+output "bastion-dns" {
+  value = "${aws_instance.bastion.public_dns}"
+}
+
+output "bastion-ip" {
+  value = "${aws_instance.bastion.public_ip}"
+}
