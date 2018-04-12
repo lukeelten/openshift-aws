d68da2b5023055e47dd10c20c8794ed410abec5c
Tobias Derksen <tobias.derksen@codecentric.de>
2018-04-12 11:58:04 +0200

    Update init scripts to install nano

--- terraform/scripts/init.sh
+++ terraform/scripts/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yum -y update
 yum -y install centos-release-openshift-origin37 epel-release
-yum -y install NetworkManager nfs-utils python36 python36-tools
+yum -y install NetworkManager nfs-utils python36 python36-tools nano
 systemctl enable NetworkManager
 systemctl start NetworkManager
 reboot
