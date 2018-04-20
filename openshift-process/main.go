f642ddc291125f0ff42a0b95439518cc9bd6a909
Tobias Derksen <tobias.derksen@codecentric.de>
2018-04-20 15:53:39 +0200

    Fixed bug in post configuration playbook

--- openshift-process/main.go
+++ openshift-process/main.go
@@ -15,6 +15,8 @@ const GEN_DIR = "generated/"
 const INVENTORY = GEN_DIR + "inventory"
 const SSH_CONFIG_FILE = GEN_DIR + "ssh.cfg"
 
+type Runner func ()
+
 func main() {
 
 	settings := configuration.ParseFlags()
@@ -44,22 +46,30 @@ func main() {
 	// @todo find better solution
 	time.Sleep(5 * time.Minute)
 
-	settings.AWSConfig.InitSession()
+	trackTime("Init AWS Session", func() {
+		settings.AWSConfig.InitSession()
+	})
 
-	sshConfig := openshift.GenerateSshConfig()
-	if err := sshConfig.WriteConfig(SSH_CONFIG_FILE); err != nil {
-		util.ExitOnError("Cannot write SSH configuration file", err)
-	}
+	trackTime("Generate SSH Config", func() {
+		sshConfig := openshift.GenerateSshConfig()
+		if err := sshConfig.WriteConfig(SSH_CONFIG_FILE); err != nil {
+			util.ExitOnError("Cannot write SSH configuration file", err)
+		}
+	})
 
-	config := openshift.GenerateConfig(SSH_CONFIG_FILE)
-	if err:= config.GenerateInventory(INVENTORY); err != nil {
-		util.ExitOnError("Cannot write OpenShift inventory file.", err)
-	}
+	trackTime("Generate OpenShift Inventory", func() {
+		config := openshift.GenerateConfig(SSH_CONFIG_FILE)
+		if err:= config.GenerateInventory(INVENTORY); err != nil {
+			util.ExitOnError("Cannot write OpenShift inventory file.", err)
+		}
+	})
 
-	persistenceConfig := openshift.NewPersistenceConfig(&settings)
-	if err := persistenceConfig.GeneratePersistenceConfigFiles(GEN_DIR); err != nil {
-		util.ExitOnError("Cannot write persistence storage configuration.", err)
-	}
+	trackTime("Generate Persistence Configuration", func() {
+		persistenceConfig := openshift.NewPersistenceConfig(&settings)
+		if err := persistenceConfig.GeneratePersistenceConfigFiles(GEN_DIR); err != nil {
+			util.ExitOnError("Cannot write persistence storage configuration.", err)
+		}
+	})
 
 	installerPath := wd + "/../openshift-ansible"
 
@@ -74,4 +84,14 @@ func main() {
 	if err := playbook.Run(INVENTORY); err != nil {
 		util.ExitOnError("Failed to run post installation configuration", err)
 	}
+}
+
+func printTime(msg string, started time.Time) {
+	elapsed := time.Since(started)
+	fmt.Printf("%s: %s", msg, elapsed)
+}
+
+func trackTime(msg string, function Runner) {
+	defer printTime(msg, time.Now())
+	function()
 }
