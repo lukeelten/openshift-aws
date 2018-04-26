4c635c15279731463214d1d6368a369a75b95133
Tobias Derksen <tobias.derksen@codecentric.de>
2018-04-26 17:16:44 +0200

    Update run to include SSH agent socket

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+agentdir=`dirname $SSH_AUTH_SOCK`
+
 docker run -it --rm \
     --name openshift-installer \
     --mount type=bind,source="$(pwd)",target=/app \
     --mount type=bind,source="$HOME/.ssh",target=/root/.ssh \
     --mount type=bind,source="$HOME/.aws",target=/root/.aws \
+    --mount type=bind,source="$agentdir",target="$agentdir" \
+    -e SSH_AUTH_SOCK="$SSH_AUTH_SOCK" \
     -w "/app/openshift-process" \
     openshift-installer \
     ./openshift-process "$@"
