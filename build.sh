64b2b3cf013f08eae2e614236f364c71d0b0a707
Tobias Derksen <tobias.derksen@codecentric.de>
2018-05-02 13:19:06 +0200

    Update build script to automatically pull newer image version

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ GOPATH=$GOPATH:`pwd`/openshift-process
 cd openshift-process && go build && cd ..
 cd openshift-ansible && git submodule update --recursive && cd ..
 
-docker build -t openshift-installer .
+docker build --pull -t openshift-installer .
