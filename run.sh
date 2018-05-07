8d27445e028123019dc863b6333cd0ed69a68789
Tobias Derksen <tobias.derksen@codecentric.de>
2018-05-07 14:53:08 +0200

    Removed unused ssh bind

--- run.sh
+++ run.sh
@@ -5,7 +5,6 @@ agentdir=`dirname $SSH_AUTH_SOCK`
 docker run -it --rm \
     --name openshift-installer \
     --mount type=bind,source="$(pwd)",target=/app \
-    --mount type=bind,source="$HOME/.ssh",target=/root/.ssh \
     --mount type=bind,source="$HOME/.aws",target=/root/.aws \
     --mount type=bind,source="$agentdir",target="$agentdir" \
     -e SSH_AUTH_SOCK="$SSH_AUTH_SOCK" \
