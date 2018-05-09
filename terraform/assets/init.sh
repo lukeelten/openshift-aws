71b98986347f35718b8ee3d38df6e5adfade1dcf
Tobias Derksen <tobias.derksen@codecentric.de>
2018-05-09 13:15:24 +0200

    Update init scripts to install needed python lib

--- terraform/assets/init.sh
+++ terraform/assets/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yum -y update
 yum -y install centos-release-openshift-origin37 epel-release
-yum -y install NetworkManager nfs-utils python36 python36-tools nano
+yum -y install NetworkManager nfs-utils python36 python36-tools nano python-passlib python2-passlib
 systemctl enable NetworkManager
 systemctl start NetworkManager
 reboot
