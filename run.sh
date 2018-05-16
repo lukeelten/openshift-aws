f0b1d163aca1308f7096d81f154cedeedcb170fe
Tobias Derksen <tobias.derksen@codecentric.de>
2018-05-16 10:03:14 +0200

    Add check for aws config dir. If it does not exists, an empty one is created to avoid errors

--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+awsdir="$HOME/.aws"
+
 if [ -z "$SSH_AUTH_SOCK" ]; then
+  exec /usr/bin/ssh-agent $SHELL
   eval $(ssh-agent)
+fi
+
+if [ ! -d "$awsdir" ]; then
+  mkdir -p $awsdir
 fi
 
 agentdir=`dirname $SSH_AUTH_SOCK`
@@ -9,7 +16,7 @@ agentdir=`dirname $SSH_AUTH_SOCK`
 docker run -it --rm \
     --name openshift-installer \
     --mount type=bind,source="$(pwd)",target=/app \
-    --mount type=bind,source="$HOME/.aws",target=/root/.aws \
+    --mount type=bind,source="$awsdir",target=/root/.aws \
     --mount type=bind,source="$agentdir",target="$agentdir" \
     -e SSH_AUTH_SOCK="$SSH_AUTH_SOCK" \
     -w "/app/openshift-process" \
