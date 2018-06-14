a2abf62479f11ced59f138886b49f18c37f67e27
Tobias Derksen <tobias.derksen@codecentric.de>
2018-06-14 15:32:34 +0200

    Added documentation to run.sh

--- run.sh
+++ run.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
 
+# Set important directories
 awsdir="$HOME/.aws"
 output="$PWD/generated"
 
+# Check if SSH Agent is running
 if [ -z "$SSH_AUTH_SOCK" ]; then
-  eval $(ssh-agent)
+    # Start SSH agent
+    eval $(ssh-agent)
 fi
 
+# Check again, fail if not running
+if [ -z "$SSH_AUTH_SOCK" ]; then
+    echo "No SSH Agent Socket found"
+    exit 1
+fi
+
+# Check if local aws configuration directory exists
 if [ ! -d "$awsdir" ]; then
-  mkdir -p $awsdir
+    # If not create one; otherwise docker command will fail
+    mkdir -p $awsdir
 fi
 
+# Check if output directory exists; if not create one
 if [ ! -d "$output" ]; then
-  mkdir "$output"
+    mkdir "$output"
 fi
 
+# local directory of ssh agent socket
 agentdir=`dirname $SSH_AUTH_SOCK`
 
+# Run docker image
 docker run -it --rm \
     --name openshift-installer \
     --mount type=bind,source="$output",target=/app/generated \
